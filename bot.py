f7dc3ce35069ccfe24613bd193f32e188a3e2d17
Joe Shufflebotham <joe.shufflebotham13@gmail.com>
2020-07-22 16:23:06 +0100

    report issues to github when it crashes

--- bot.py
+++ bot.py
@@ -13,60 +13,87 @@ bot = Bot(":")
 
 
 async def create_permissions(guild):
-    gm_role = next((x for x in guild.roles if x.name == "GameMaster"), None)
-    player_role = next((x for x in guild.roles if x.name == "Explorer"), None)
+    try:
+        gm_role = next((x for x in guild.roles if x.name == "GameMaster"), None)
+        player_role = next((x for x in guild.roles if x.name == "Explorer"), None)
 
-    if gm_role is None:
-        await guild.create_role(name="GameMaster", color=discord.Color(0xff9000),
-                                reason="Used to manage SteveBot permissions")
+        if gm_role is None:
+            await guild.create_role(name="GameMaster", color=discord.Color(0xff9000),
+                                    reason="Used to manage SteveBot permissions")
 
-    if player_role is None:
-        await guild.create_role(name="Explorer", color=discord.Color(0x009929),
-                                reason="Used to manage SteveBot permissions")
+        if player_role is None:
+            await guild.create_role(name="Explorer", color=discord.Color(0x009929),
+                                    reason="Used to manage SteveBot permissions")
+    except Exception as e:
+        from git.github_connection import report_error_to_repo
+        report_error_to_repo(bot, e)
 
 
 @bot.event
 async def on_ready():
-    print("on READY")
-    await bot.change_presence(status=discord.Status.online,
-                              activity=discord.Game(name="you"))
+    try:
+        print("on READY")
+        await bot.change_presence(status=discord.Status.online,
+                                  activity=discord.Game(name="you"))
 
-    for guild in bot.guilds:
-        create_game_if_not_in_guild(guild)
-        await create_permissions(guild)
+        for guild in bot.guilds:
+            create_game_if_not_in_guild(guild)
+            await create_permissions(guild)
+
+    except Exception as e:
+        from git.github_connection import report_error_to_repo
+        report_error_to_repo(bot, e)
 
 
 @bot.event
 async def on_guild_join(guild):
-    print("Joined a guild")
-    create_game_if_not_in_guild(guild)
-    await create_permissions(guild)
+    try:
+        print("Joined a guild")
+        create_game_if_not_in_guild(guild)
+        await create_permissions(guild)
+    except Exception as e:
+        from git.github_connection import report_error_to_repo
+        report_error_to_repo(bot, e)
 
 
 @bot.event
 async def on_message(message):
-    if message.author == bot.user:
-        return
-
-    ##If it's the live bot on the test server, ignore!
-    if message.guild is not None:
-        if str(message.guild.id) == "403277441650393099" and str(bot.user.id) == "731917492498333767":
+    try:
+        if message.author == bot.user:
             return
 
-    await process_command(bot, message)
+        ##If it's the live bot on the test server, ignore!
+        if message.guild is not None:
+            if str(message.guild.id) == "403277441650393099" and str(bot.user.id) == "731917492498333767":
+                return
+
+        await process_command(bot, message)
+    except Exception as e:
+        from git.github_connection import report_error_to_repo
+        issue = report_error_to_repo(bot, e)
+
+        if issue is not None:
+            embed = discord.Embed(title="Error", color=0xff0000, description=
+                                  "Something went wrong, issue reported to github [here](" + issue.html_url + ")")
+
+            await message.channel.send(embed=embed)
 
 
 @bot.event
 async def on_raw_reaction_add(payload):
-    channel = await bot.fetch_channel(payload.channel_id)
-    user = await channel.guild.fetch_member(payload.user_id)
-    message = await channel.fetch_message(payload.message_id)
+    try:
+        channel = await bot.fetch_channel(payload.channel_id)
+        user = await channel.guild.fetch_member(payload.user_id)
+        message = await channel.fetch_message(payload.message_id)
 
-    if payload.user_id == bot.user.id:
-        return
+        if payload.user_id == bot.user.id:
+            return
 
-    from commands.improve import check_improvements_from_reaction
-    await check_improvements_from_reaction(payload.emoji, user, message)
+        from commands.improve import check_improvements_from_reaction
+        await check_improvements_from_reaction(payload.emoji, user, message)
+    except Exception as e:
+        from git.github_connection import report_error_to_repo
+        report_error_to_repo(bot, e)
 
 
 token = ""
---
+++ git/github_connection.py
@@ -0,0 +1,50 @@
+from github import Github
+import os
+import traceback
+from difflib import SequenceMatcher
+
+cwd = os.getcwd()
+
+
+def similar(a, b):
+    return SequenceMatcher(None, a, b).ratio()
+
+
+def report_error_to_repo(bot, thrown):
+    access_token = ""
+
+    with open(cwd + '/git/git_access_token.txt', 'r') as myfile:
+        access_token = myfile.read()
+
+    try:
+        g = Github(access_token)
+        repo = g.get_repo("joeShuff/discord-RPGSteveBot")
+        bot_label = repo.get_label("bot reported issue")
+
+        thrown_issue = ''.join(traceback.format_exception(etype=type(thrown), value=thrown, tb=thrown.__traceback__))
+        report = "Error collected from " + str(bot.user.name) + "\n```" + thrown_issue + "```"
+
+        all_issues = repo.get_issues()
+
+        alreadyReported = None
+
+        for issue in all_issues:
+            if similar(issue.body, report) > 0.95:
+                alreadyReported = issue
+                issue.create_comment(body="+1\n\n" + report)
+
+        if alreadyReported is not None:
+            print("+1d existing issue")
+            return alreadyReported
+
+        issue = repo.create_issue(
+            title="Error from Bot",
+            body=report,
+            labels=[bot_label],
+            assignee="joeShuff"
+        )
+
+        print("Reported error to github")
+        return issue
+    except:
+        return None
